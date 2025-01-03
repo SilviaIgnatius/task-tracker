f38b593abbb570858cd834bbb00f7dcef3ed4b53
SilviaIgnatius <silviaharlin@gmail.com>
2025-01-03 18:01:54 +0530

    Database Factories and Models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,17 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create()->each(function ($user) {
+            Task::factory(5)->create([
+                'user_id' => $user->id,
+            ]);
+        });
 
         User::factory()->create([
             'name' => 'Test User',
